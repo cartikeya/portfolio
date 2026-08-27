f64df5ba1b4b33cec48b3a2ac86b4a4fe928d0df
cartikeya <cartikeya.official@gmail.com>
2026-08-27 19:39:53 +0530

    Fix Vite base path for custom domain and add CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/", // Replace with your exact GitHub repo name
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
